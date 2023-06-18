17774739270535b04ba29c28c8f41d125ccd98ea
Marco Laura <marco.laura@gmail.com>
2023-06-18 01:32:57 -0400

    Ajustes de parametro pantalla sobre SNISE

--- common/mixins/constants.js
+++ common/mixins/constants.js
@@ -2,4 +2,5 @@
 export default {
   version: 'v1.0',
   numeroMinimoBusqueda: 3,
+  TIPOS_PARAMETROS: [ 'NOSOTROS', 'BANNERS', 'MANUAL_ADMIN', 'MANUAL_OBSCD', 'MANUAL_ENTIDAD', 'MANUAL_REPORTES'],
 };
--- components/section/snise/SobreSnise.vue
+++ components/section/snise/SobreSnise.vue
@@ -5,21 +5,76 @@
         <v-row justify="center">
           <v-col cols="12" sm="7" lg="5" class="d-flex align-center">
             <div class="text-center text-sm-left">
-              <h2 class="formbanner1-title font-weight-bold">
-                SNISE
-              </h2>
-              <p class="mt-6 pt-4 text-muted">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-              </p>
+              <div class="text-center text-sm-left" v-html="sobreSnise?.descripcion"></div>
             </div>
           </v-col>
           <v-col cols="12" sm="5" lg="6" class="ml-auto">
-            <img src="/images/form-banner/quienesSomos.jpg" alt="banner" class="img-fluid"/>
+            <!-- <img src="/images/form-banner/quienesSomos.jpg" alt="banner" class="img-fluid"/> -->
+            <img :src="urlRemote" alt="banner" class="img-fluid" />
           </v-col>
         </v-row>
       </v-container>
     </div>
   </div>
 </template>
+<script>
+import constants from '@/common/mixins/constants';
 
+const runtimeConfig = useRuntimeConfig();
 
+export default {
+  name: "SobreSnise",
+  data() {
+    return {
+      sobreSnise: null,
+      urlRemote: null,
+      constants,
+      runtimeConfig: runtimeConfig,
+    };
+  },
+  created() {
+    this.initialize();
+  },
+  methods: {
+    async initialize() {
+      try {
+        const rta = await useFetch(`${this.runtimeConfig?.public?.apiBase}publico/parametros/${this.constants.TIPOS_PARAMETROS[0]}`);
+
+        if (rta.data?._rawValue) {
+          this.sobreSnise = rta.data?._rawValue[0];
+          this.urlRemote = await this.getImage(this.sobreSnise.url);
+        }
+      } catch (error) {
+        console.log('=======================> Error', error);
+      };
+    },
+    async getImage(url) {
+      try {
+        const settings = {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ url }),
+        };
+        const result = await fetch(`${this.runtimeConfig?.public?.apiBase}publico/archivo-image`, settings)
+          .then((response) => {
+          return response.arrayBuffer();
+        });
+        if (result) {
+          const b64 = btoa(String.fromCharCode(...new Uint8Array(result)));
+          const imgData = "data:image/jpeg;base64," + b64;
+          return imgData;
+        }
+        return null;
+      } catch (error) {
+        console.log('========================> error', error);
+        this.snackbar = true;
+        this.textSnackbar = "Ocurrió un error, intente nuevamente.";
+        this.colorSnackbar = "red";
+      }
+    },
+  },
+};
+</script>
